4109c734f3577013d8af100264759d7486fd7376
Karsten Ahnert <karsten.ahnert@gmx.de>
2015-05-15 15:16:11 +0200

    enabling coveralls

--- .ci/commit_coveralls.sh
+++ .ci/commit_coveralls.sh
@@ -3,6 +3,6 @@ set -e
 
 cd build
 # lcov --zerocounters --directory test
-lcov --directory test --base-directory ../include/cit --capture --output-file coverage.info
+lcov --directory test --base-directory $CIT_ROOT/include/cit --capture --output-file coverage.info
 lcov --remove coverage.info '/usr*' '*/third_party*' -o coverage.info
 coveralls-lcov coverage.info
