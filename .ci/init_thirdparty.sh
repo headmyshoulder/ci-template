b43932d9ad2d28d96a482aa7139672be2c515fc1
Karsten Ahnert <karsten.ahnert@gmx.de>
2016-03-04 13:08:21 +0100

    fixing boost build

--- .ci/init_thirdparty.sh
+++ .ci/init_thirdparty.sh
@@ -14,14 +14,12 @@ export BOOST_ROOT="${THIRD_PARTY_ROOT}/boost"
 cd $BOOST_ROOT
 ./bootstrap.sh
 
-echo "using gcc ; " >> project-config.jam
 echo "using gcc : 4.9 : g++-4.9 ;" >> project-config.jam
 echo "using gcc : 5.0 : g++-5.0 ;" >> project-config.jam
 echo "using gcc : 5.1 : g++-5.1 ;" >> project-config.jam
 echo "using gcc : 5.2 : g++-5.2 ;" >> project-config.jam
 echo "using gcc : 5.3 : g++-5.3 ;" >> project-config.jam
 echo "using gcc : 6.0 : g++-6.0 ;" >> project-config.jam
-echo "\n\n" >> project-config.jam
 echo "using clang : 3.5 : clang++3.5 ;" >> project-config.jam
 echo "using clang : 3.6 : clang++3.6 ;" >> project-config.jam
 echo "using clang : 3.7 : clang++3.7 ;" >> project-config.jam
@@ -32,4 +30,4 @@ ADDITIONAL_FLAGS="-std=c+14"
 if [ "$LIBCXX" == "on" ]; then
   ADDITIONAL_FLAGS="${ADDITIONAL_FLAGS} -stdlib=libc++"
 fi
-./b2 --with-thread --with-system --with-program_options -d0 --toolset=$CXX --cxxflags=""
+./b2 --with-thread --with-system --with-program_options -d0 --toolset=$CXX --cxxflags="${ADDITIONAL_FLAGS}"
