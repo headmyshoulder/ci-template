aaef4a2c6ec8bd8e27a4fcd52f28cfa7c79b54e7
Karsten Ahnert <karsten.ahnert@gmx.de>
2016-03-04 10:11:40 +0100

    adding c++ example with boost program options

--- examples/example1.cpp
+++ examples/example1.cpp
@@ -9,15 +9,45 @@
 
 #include <cit/api.hpp>
 
+#include <boost/program_options.hpp>
 
 #include <iostream>
 
-using namespace std;
+namespace po = boost::program_options;
 
+
+
+inline auto get_options( void )
+{
+    po::options_description options( "Options" );
+    options.add_options()
+    ( "normalize" , po::value< bool >() , "normalize the input data" )
+    ;
+    return options;
+}
 
 int main( int argc , char *argv[] )
 {
-    cout << cit::foo1( 10 ) << endl;
-    cout << cit::foo2( 10 ) << endl;
+    auto options = get_options();
+    
+    po::options_description cmdline_options;
+    cmdline_options.add( options );
+    
+    po::variables_map vm;
+    try
+    {
+        po::store( po::command_line_parser( argc , argv ).options( cmdline_options ).run() , vm );
+        po::notify( vm );
+    }
+    catch( std::exception& e )
+    {
+        std::cerr << "Error " << e.what() << "\n\n";
+        std::cerr << cmdline_options << "\n";
+        return -1;
+    }
+
+
+    std::cout << cit::foo1( 10 ) << std::endl;
+    std::cout << cit::foo2( 10 ) << std::endl;
     return 0;
 }
