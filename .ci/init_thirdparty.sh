be32f0d00f88599324081255538843e9680f6bd1
Karsten Ahnert <karsten.ahnert@gmx.de>
2016-03-04 13:25:50 +0100

    fixing boost build

--- .ci/init_thirdparty.sh
+++ .ci/init_thirdparty.sh
@@ -14,12 +14,15 @@ export BOOST_ROOT="${THIRD_PARTY_ROOT}/boost"
 cd $BOOST_ROOT
 ./bootstrap.sh
 
+
 if [ -n "$GCC_VERSION" ]; then  
   echo "using gcc : ${GCC_VERSION} : ${CXX} ;" >> project-config.jam
+  TOOLSET="gcc-${GCC_VERSION}"
 fi
 
 if [ -n "$CLANG_VERSION" ]; then
     echo "using clang ; ${CLANG_VERSION} : ${CXX} ;" >> project-config.jam
+    TOOLSET="clang-${CLANG_VERSION}"
 fi
 
 
@@ -27,4 +30,4 @@ ADDITIONAL_FLAGS="-std=c+14"
 if [ "$LIBCXX" == "on" ]; then
   ADDITIONAL_FLAGS="${ADDITIONAL_FLAGS} -stdlib=libc++"
 fi
-./b2 --with-thread --with-system --with-program_options -d0 --toolset=$CXX --cxxflags="${ADDITIONAL_FLAGS}"
+./b2 --with-thread --with-system --with-program_options -d0 --toolset=${TOOLSET} --cxxflags="${ADDITIONAL_FLAGS}"
