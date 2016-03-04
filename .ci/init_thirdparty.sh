d84b45a5af248ea9dbdd65b821d973a584420285
Karsten Ahnert <karsten.ahnert@gmx.de>
2016-03-04 13:57:10 +0100

    fixing boost build

--- .ci/init_thirdparty.sh
+++ .ci/init_thirdparty.sh
@@ -21,7 +21,7 @@ if [ -n "$GCC_VERSION" ]; then
 fi
 
 if [ -n "$CLANG_VERSION" ]; then
-    echo "using clang ; ${CLANG_VERSION} : ${CXX} ;" >> project-config.jam
+    echo "using clang : ${CLANG_VERSION} : ${CXX} ;" >> project-config.jam
     TOOLSET="clang-${CLANG_VERSION}"
 fi
 
@@ -30,4 +30,4 @@ ADDITIONAL_FLAGS="-std=c+14"
 if [ "$LIBCXX" == "on" ]; then
   ADDITIONAL_FLAGS="${ADDITIONAL_FLAGS} -stdlib=libc++"
 fi
-./b2 --with-thread --with-system --with-program_options -d0 --toolset=${TOOLSET} --cxxflags="${ADDITIONAL_FLAGS}"
+./b2 --with-thread --with-system --with-program_options -d0 --toolset=${TOOLSET} --cxxflags="${ADDITIONAL_FLAGS} ${CXX_FLAGS}" --linkflags="${LDD_FLAGS}"
