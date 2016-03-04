316136f350ec81c4d8aaf30abb194fa7ba79ef1d
Karsten Ahnert <karsten.ahnert@gmx.de>
2016-03-04 23:33:13 +0100

    trying to fix libcxx

--- .ci/build_and_test.sh
+++ .ci/build_and_test.sh
@@ -10,6 +10,9 @@ export CMAKE_OPTIONS="-DCMAKE_BUILD_TYPE=$BUILD_TYPE"
 #     export CMAKE_OPTIONS="$CMAKE_OPTIONS -DTEST_COVERAGE=ON"
 # fi
 
+ldd ../../third_party/boost/stage/lib/libboost_program_options.so
+ldd CMakeFiles/example1.dir/example1.cpp.o
+
 cmake .. $CMAKE_OPTIONS
 make -j 3 VERBOSE=1
 
