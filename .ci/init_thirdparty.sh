cea0b6d48d178466f17fcc8a8d4f5cc883d3562b
Karsten Ahnert <karsten.ahnert@gmx.de>
2016-03-06 13:58:10 +0100

    fixing now gcc build

--- .ci/init_thirdparty.sh
+++ .ci/init_thirdparty.sh
@@ -26,8 +26,12 @@ fi
 
 
 export OPTIONS=""
-if [ -n "$CXXFLAGS" ]; then export OPTIONS="$OPTIONS cxxflags="-std=c++14 $CXXFLAGS" fi
-if [-n "$LDFLAGS" ]; then export OPTIONS="$OPTIONS linkflags=$LDFLAGS" fi
+if [ -n "$CXXFLAGS" ]; then export
+    OPTIONS="$OPTIONS cxxflags="-std=c++14 $CXXFLAGS"
+fi
+if [ -n "$LDFLAGS" ]; then
+    export OPTIONS="$OPTIONS linkflags=$LDFLAGS"
+fi
 
 
 ./b2 --with-thread --with-system --with-program_options -d0 toolset=${TOOLSET} $OPTIONS
