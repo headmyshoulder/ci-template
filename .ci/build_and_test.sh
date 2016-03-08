38624369c4a4521e85eaefb9f7153d26792b0f7d
Karsten Ahnert <karsten.ahnert@gmx.de>
2016-03-08 07:38:23 +0100

    adding coverage options

--- .ci/build_and_test.sh
+++ .ci/build_and_test.sh
@@ -5,10 +5,10 @@ cd build
 
 export CMAKE_OPTIONS="-DCMAKE_BUILD_TYPE=$BUILD_TYPE"
 
-# if [ -n "COVERALLS_BUILD" ];
-# then
-#     export CMAKE_OPTIONS="$CMAKE_OPTIONS -DTEST_COVERAGE=ON"
-# fi
+if [ -n "COVERALLS_BUILD" ];
+then
+    export CMAKE_OPTIONS="$CMAKE_OPTIONS -DTEST_COVERAGE=ON"
+fi
 
 cmake .. $CMAKE_OPTIONS
 make -j 3
@@ -16,11 +16,12 @@ make -j 3
 
 
 ctest -V
+./examples/example1.cpp
 
 
 
-# if [ -n "$RUN_VALGRIND" ];
-# then 
-#     VALGRIND_CMD="valgrind --leak-check=full --show-reachable=yes --error-exitcode=1 "
-#     $VALGRIND_CMD test/cit_tests
-# fi
+if [ -n "$RUN_VALGRIND" ];
+then 
+    VALGRIND_CMD="valgrind --leak-check=full --show-reachable=yes --error-exitcode=1 "
+    $VALGRIND_CMD test/cit_tests
+fi
