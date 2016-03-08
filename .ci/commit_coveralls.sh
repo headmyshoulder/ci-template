c72deeb64ecdf468d38c93c361f9f993490c8651
Karsten Ahnert <karsten.ahnert@gmx.de>
2016-03-08 09:12:08 +0100

    trying to fix boost build

--- .ci/commit_coveralls.sh
+++ .ci/commit_coveralls.sh
@@ -3,6 +3,6 @@ set -e
 
 cd build
 # lcov --zerocounters --directory test
-lcov --gcov-tool=$CGOV --directory test --base-directory $CIT_ROOT/include/cit --capture --output-file coverage.info
+lcov --gcov-tool=$GCOV --directory test --base-directory $CIT_ROOT/include/cit --capture --output-file coverage.info
 lcov --remove coverage.info '/usr*' '*/third_party*' -o coverage.info
 coveralls-lcov coverage.info
