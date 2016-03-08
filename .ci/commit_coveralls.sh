1ba6d20d63df1eb2d5b047ee998bb3b2b5d0ab9d
Karsten Ahnert <karsten.ahnert@gmx.de>
2016-03-08 08:19:06 +0100

    adding coverage options

--- .ci/commit_coveralls.sh
+++ .ci/commit_coveralls.sh
@@ -3,6 +3,6 @@ set -e
 
 cd build
 # lcov --zerocounters --directory test
-lcov --directory test --base-directory $CIT_ROOT/include/cit --capture --output-file coverage.info
+lcov --gcov_tool=$CGOV --directory test --base-directory $CIT_ROOT/include/cit --capture --output-file coverage.info
 lcov --remove coverage.info '/usr*' '*/third_party*' -o coverage.info
 coveralls-lcov coverage.info
